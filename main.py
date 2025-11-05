be4b7f4834497ff7f440b97bbe9d6da4da41f899
juananestival <juananestival@gmail.com>
2025-11-05 10:02:25 +0000

    sequence updater implemented

--- main.py
+++ main.py
@@ -4,29 +4,6 @@ from simple_websocket import Server, ConnectionClosed
 
 app = Flask(__name__)
 
-def sequence_updater(myseq, audioconnectorseq):
-    """
-        When Genesys send a request from audioconnector then Genesys is the client and the target
-        is the server. 
-        Example:
-        When audioconnecto send an open request the server will recive something like this
-        {"seq":1,"serverseq":0}
-
-        Now the server must to answer with some mandatory fields
-        - type
-        - version
-        - id
-        - "seq":1,
-        - "clientseq":1, 
-
-        In this case we are the clientseq. 
-        We must to answer like to the genesys request "seq1" we respond with "clientseq:1"
-
-    """
-    clientseq = myseq + 1
-    seq = audioconnectorseq
-
-    return clientseq, seq
 
 
 @app.route('/ws/<path:path_param>', websocket=True)
@@ -44,19 +21,20 @@ def echo(path_param):
                     # Extract the value of the 'type' key
                     message_type = message.get('type')
                     print(f"Successfully extracted message type: '{message_type}'")
-                    clientseq, audioconnectorseq = sequence_updater(message.get('serverseq'), message.get('seq'))
-                    print(f"the pair of sequende is {clientseq} and {audioconnectorseq}")
 
                     if message_type == 'open':
+      
                         idtest = message.get('id')
+                        myseq = message.get('serverseq')
+                        myseq = myseq + 1
                         #current_client_seq = message.get('seq')
                         #current_server_seq = message.get('serverseq')
                         data_to_send = {
                             "type": "opened",
                             "id": idtest,
                             "version": "2",
-                            "seq":audioconnectorseq,
-                            "clientseq":clientseq, 
+                            "seq":myseq,
+                            "clientseq":message.get('seq'), 
                             "parameters": {
                             "startPaused": True,
                             "media": [
@@ -70,6 +48,35 @@ def echo(path_param):
                             }
                             ],
                             "language": "es-us",
+                        }
+                        }
+                        ws.send(json.dumps(data_to_send))
+
+                    if message_type == 'ping':
+                        idtest = message.get('id')
+
+                        #current_client_seq = message.get('seq')
+                        #current_server_seq = message.get('serverseq')
+                        myseq = message.get('serverseq')
+                        myseq = myseq + 1
+                        print(f"myseq {myseq}")
+                        data_to_send = {
+                            "type": "pong",
+                            "id": idtest,
+                            "version": "2",
+                            "seq":myseq,
+                            "clientseq":message.get('seq'), 
+                            "parameters": {
+                            "media": [
+                            {
+                                "type": "audio",
+                                "format": "PCMU",
+                                "channels": [
+                                "external"
+                                ],
+                                "rate": 8000
+                            }
+                            ]
                         }
                         }
                         ws.send(json.dumps(data_to_send))
